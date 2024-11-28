0f89ba86dec9b4cde27580e073e2206399bc9d11
xlWang1 <155468096+xlWang1@users.noreply.github.com>
2024-11-28 22:18:53 +0800

    Update CreateObj.java

--- src/main/java/com/wxl/util/CreateObj.java
+++ src/main/java/com/wxl/util/CreateObj.java
@@ -45,54 +45,43 @@ public class CreateObj {
         // 生成精英敌机
         if (planeWar.gameTimer.getElapsedSeconds()> 10 && eliteEnemyObjNum < 3) {
             if (eliteEnemyObjNum == 0) {
-                planeWar.eliteEnemyObj = new EliteEnemyObj(GameUtil.eliteEnemyShip1, 150, 0, 80,GameUtil.eliteEnemyShip1.getWidth(), GameUtil.eliteEnemyShip1.getHeight(),5,planeWar);
-                GameUtil.eliteEnemyShipList.add(planeWar.eliteEnemyObj);
+                EliteEnemyObj eliteEnemyObj1 = new EliteEnemyObj(GameUtil.eliteEnemyShip1, 150, 0, 80,GameUtil.eliteEnemyShip1.getWidth(), GameUtil.eliteEnemyShip1.getHeight(),5,planeWar);
+//                planeWar.eliteEnemyObjLists.add(eliteEnemyObj1);
+                GameUtil.eliteEnemyShipList.add(eliteEnemyObj1);
                 eliteEnemyObjNum = 1;
                 GameUtil.AllObjList.add(GameUtil.eliteEnemyShipList.get(GameUtil.eliteEnemyShipList.size()-1));
-            } else if (planeWar.gameTimer.getElapsedSeconds()> 30 && eliteEnemyObjNum < 2){
-                planeWar.eliteEnemyObj = new EliteEnemyObj(GameUtil.eliteEnemyShip1, 180, 0, 130,GameUtil.eliteEnemyShip1.getWidth(), GameUtil.eliteEnemyShip1.getHeight(),5,planeWar);
-                GameUtil.eliteEnemyShipList.add(planeWar.eliteEnemyObj);
+            } else if (planeWar.gameTimer.getElapsedSeconds()> 20 && eliteEnemyObjNum < 2){
+                EliteEnemyObj eliteEnemyObj2 = new EliteEnemyObj(GameUtil.eliteEnemyShip1, 180, 0, 130,GameUtil.eliteEnemyShip1.getWidth(), GameUtil.eliteEnemyShip1.getHeight(),5,planeWar);
+                GameUtil.eliteEnemyShipList.add(eliteEnemyObj2);
                 eliteEnemyObjNum = 2;
                 GameUtil.AllObjList.add(GameUtil.eliteEnemyShipList.get(GameUtil.eliteEnemyShipList.size()-1));
             } else if (planeWar.boss != null) {
-                planeWar.eliteEnemyObj = new EliteEnemyObj(GameUtil.eliteEnemyShip1, 200, 0, 80,GameUtil.eliteEnemyShip1.getWidth(), GameUtil.eliteEnemyShip1.getHeight(),5,planeWar);
-                GameUtil.eliteEnemyShipList.add(planeWar.eliteEnemyObj);
+                EliteEnemyObj eliteEnemyObj3 = new EliteEnemyObj(GameUtil.eliteEnemyShip1, 200, 0, 80,GameUtil.eliteEnemyShip1.getWidth(), GameUtil.eliteEnemyShip1.getHeight(),5,planeWar);
+                GameUtil.eliteEnemyShipList.add(eliteEnemyObj3);
                 eliteEnemyObjNum = 3;
                 GameUtil.AllObjList.add(GameUtil.eliteEnemyShipList.get(GameUtil.eliteEnemyShipList.size()-1));
             }
         }
-        // 生成boss子弹
-//        if (planeWar.count % 20 == 0 && planeWar.boss != null) {
-//            if (planeWar.boss.BossHp <= planeWar.boss.BossMAXHp/2){
-//                if (!planeWar.bossBulletUp){
-//                    GameUtil.enemyBulletList.clear();
-//                    planeWar.bossBulletUp = true;
-//                }
-//                GameUtil.enemyBulletList.add(new EnemyBulletObj(GameUtil.eneMyBulletLevel2,planeWar.boss.x+35,planeWar.boss.y-20,24,16,10,planeWar,15));
-//                GameUtil.AllObjList.add(GameUtil.enemyBulletList.get(GameUtil.enemyBulletList.size()-1));
-//            }
-//            else {
-//                GameUtil.enemyBulletList.add(new EnemyBulletObj(GameUtil.eneMyBulletLevel1,planeWar.boss.x+35,planeWar.boss.y-20,16,16,8,planeWar,15));
-//                GameUtil.AllObjList.add(GameUtil.enemyBulletList.get(GameUtil.enemyBulletList.size()-1));
-//            }
-//        }
-        // 生成boss子弹
+        // 生成敌方子弹
         if (planeWar.count % 20 == 0) {
+
             if (planeWar.boss != null && planeWar.boss.BossHp <= planeWar.boss.BossMAXHp/2){
                 if (!planeWar.bossBulletUp){
                     GameUtil.enemyBulletList.clear();
                     planeWar.bossBulletUp = true;
                 }
-                GameUtil.enemyBulletList.add(new EnemyBulletObj(GameUtil.eneMyBulletLevel2,planeWar.boss.x+35,planeWar.boss.y-20,24,16,10,planeWar,15));
+                GameUtil.enemyBulletList.add(new EnemyBulletObj(GameUtil.eneMyBulletLevel2,planeWar.boss.x+35,planeWar.boss.y-20,24,16,10,planeWar,30));
+                GameUtil.AllObjList.add(GameUtil.enemyBulletList.get(GameUtil.enemyBulletList.size()-1));
+            } else if (planeWar.boss != null){
+                GameUtil.enemyBulletList.add(new EnemyBulletObj(GameUtil.eneMyBulletLevel1,planeWar.boss.x+35,planeWar.boss.y-20,16,16,8,planeWar,15));
+                GameUtil.AllObjList.add(GameUtil.enemyBulletList.get(GameUtil.enemyBulletList.size()-1));
             }
-            else {
-                if (planeWar.boss != null){
-                    GameUtil.enemyBulletList.add(new EnemyBulletObj(GameUtil.eneMyBulletLevel1,planeWar.boss.x+35,planeWar.boss.y-20,16,16,8,planeWar,15));
-                }else if (planeWar.eliteEnemyObj != null){
-                    GameUtil.enemyBulletList.add(new EnemyBulletObj(GameUtil.eneMyBulletLevel1,planeWar.eliteEnemyObj.x+22,planeWar.eliteEnemyObj.y+57,16,16,8,planeWar,15));
-                }
-                if (GameUtil.enemyBulletList.size() >= 1) {
-                    GameUtil.AllObjList.add(GameUtil.enemyBulletList.get(GameUtil.enemyBulletList.size()-1));
+            if (GameUtil.eliteEnemyShipList.size() >= 1){
+                for(EliteEnemyObj e : GameUtil.eliteEnemyShipList) {
+                    if(e.EliteEnemyHp > 0){
+                        GameUtil.enemyBulletList.add(new EnemyBulletObj(GameUtil.eneMyBulletLevel1,e.getX()+22,e.getX()+57,16,16,8,planeWar,15));
+                        GameUtil.AllObjList.add(GameUtil.enemyBulletList.get(GameUtil.enemyBulletList.size()-1));
+                    }
                 }
 
             }
